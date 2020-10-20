ad27f6742e422daaabb5a1685ef83cac24ed71fc
Emmanuel Huitron <emanlahdy125@gmail.com>
2020-10-20 13:37:03 -0500

    added movement to character with WASD

---
+++ SpacePirates/Assets/C#Script/Walk.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour
+{
+    Animator anim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        {
+            float move = Input.GetAxis("Horizontal");
+            anim.SetFloat("Speed", move);
+        }
+    }
+
+    void Movement()
+    {
+        if(Input.GetKey (KeyCode.W))
+        {
+            transform.Translate(Vector2.up * 3f * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+        if(Input.GetKey (KeyCode.A))
+        {
+            transform.Translate(Vector2.left * 3f * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+        if(Input.GetKey (KeyCode.S))
+        {
+            transform.Translate(Vector2.down * 3f * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+        if(Input.GetKey (KeyCode.D))
+        {
+            transform.Translate(Vector2.right * 3f * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+    }
+}
