c7ba03ee9edbc26b4ac97732a37b736b2404d08b
Randall178 <Hinojos99@live.com>
2020-11-23 05:19:57 -0600

    Able to create paths for Ai's (no animation yet)

---
+++ SpacePirates/Assets/C#Script/Path.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour{
+ public Transform[] target;
+ public float speed;
+
+ private int current;
+
+ void Update (){
+     if (transform.position != target[current].position)
+     {
+         Vector2 pos = Vector2.MoveTowards(transform.position,target[current].position, speed * Time.deltaTime);
+         GetComponent<Rigidbody2D>().MovePosition(pos);
+     }
+     else current = (current + 1) % target.Length;
+ }
+}
